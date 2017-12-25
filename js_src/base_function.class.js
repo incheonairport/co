38ab84b06eb09999585d73d0f6a5326fb907513b
ministori <ministori@gmail.com>
2017-12-25 23:42:51 +0900

    v1.1.31 2017/12/25 공사메인 추가 수정

--- js_src/base_function.class.js
+++ js_src/base_function.class.js
@@ -2,7 +2,7 @@
  * Base Class *
  **************/
 
-var Index, HeaderGnb;
+var Index, HeaderGnb, MainVisual, BottomBanner;
 
 $(function(){
 
@@ -12,23 +12,9 @@ $(function(){
 
   Index = function(){
 
-    this.$mainSection = $('.full-page-content .section');
     this.$mainVisualItem = $('.main-visual-item');
-    this.$mainFullPageContent = $('.full-page-content');
     this.currentMainSectionIndex = 0;
     this.easingType = 'easeInOutExpo';
-
-    this.setCurrentMainSectionIndex = function(currentIndex){
-
-      this.currentMainSectionIndex = currentIndex;
-
-    };
-
-    this.getCurrentMainSectionIndex = function(){
-
-      return this.currentMainSectionIndex;
-
-    };
 
   };
 
@@ -36,55 +22,55 @@ $(function(){
    * HeaderGnb Class
    */
 
-  HeaderGnb = new function(){
-
-    Index.apply(this);
-
-    var $visualItem = this.$mainVisualItem;
-    var $mainSection = this.$mainSection;
-    var $fullPageContent = this.$mainFullPageContent;
-
-
-    var _initClass = function(){
-      $('.header, .gnb').addClass( $fullPageContent.find('.section').eq(0).data('gnb-color') );
-    };
-
-    var _setClassVisual = function(index){
-      //console.log('set visual index : ' + index);
-      $('.header').attr('class', 'header ' + $visualItem.eq(index).data('gnb-color') );
-      $('.gnb').attr('class', 'gnb ' + $visualItem.eq(index).data('gnb-color') );
-    };
-
-    var _setClassSection = function(index){
-      $('.header').attr('class', 'header ' + $mainSection.eq(index).data('gnb-color') );
-      $('.gnb').attr('class', 'gnb ' + $mainSection.eq(index).data('gnb-color') );
-      //console.log('set section index : ' + index);
-    };
-
-    this.setClass = function(setClassSectionIndex, setClassVisualIndex){
-
-      //console.log('section index : ' + setClassSectionIndex);
-      //console.log('visual index : ' + setClassVisualIndex);
-
-      this.setCurrentMainSectionIndex(setClassSectionIndex);
-
-      if( setClassSectionIndex == 0 ){
-
-        //console.log('set visual');
-        _setClassVisual(setClassVisualIndex);
-
-      } else {
-
-        //console.log('set section');
-        _setClassSection(setClassSectionIndex);
-
-      }
-
-    };
-
-    _initClass();
-
-  };
+  //HeaderGnb = new function(){
+  //
+  //  Index.apply(this);
+  //
+  //  var $visualItem = this.$mainVisualItem;
+  //  var $mainSection = this.$mainSection;
+  //  var $fullPageContent = this.$mainFullPageContent;
+  //
+  //
+  //  var _initClass = function(){
+  //    $('.header, .gnb').addClass( $fullPageContent.find('.section').eq(0).data('gnb-color') );
+  //  };
+  //
+  //  var _setClassVisual = function(index){
+  //    //console.log('set visual index : ' + index);
+  //    $('.header').attr('class', 'header ' + $visualItem.eq(index).data('gnb-color') );
+  //    $('.gnb').attr('class', 'gnb ' + $visualItem.eq(index).data('gnb-color') );
+  //  };
+  //
+  //  var _setClassSection = function(index){
+  //    $('.header').attr('class', 'header ' + $mainSection.eq(index).data('gnb-color') );
+  //    $('.gnb').attr('class', 'gnb ' + $mainSection.eq(index).data('gnb-color') );
+  //    //console.log('set section index : ' + index);
+  //  };
+  //
+  //  this.setClass = function(setClassSectionIndex, setClassVisualIndex){
+  //
+  //    //console.log('section index : ' + setClassSectionIndex);
+  //    //console.log('visual index : ' + setClassVisualIndex);
+  //
+  //    this.setCurrentMainSectionIndex(setClassSectionIndex);
+  //
+  //    if( setClassSectionIndex == 0 ){
+  //
+  //      //console.log('set visual');
+  //      _setClassVisual(setClassVisualIndex);
+  //
+  //    } else {
+  //
+  //      //console.log('set section');
+  //      _setClassSection(setClassSectionIndex);
+  //
+  //    }
+  //
+  //  };
+  //
+  //  _initClass();
+  //
+  //};
 
   /**
    * MainVisual Class
@@ -307,6 +293,139 @@ $(function(){
     // running in constructor when loading
     _init();
     this.rollAuto();
+
+  };
+
+  /**
+   * BottomBanner Class
+   */
+
+  BottomBanner = new function(){
+
+    Index.apply(this);
+
+    // private
+    var currentVisualIndex = 0;
+    var nextVisualIndex = 0;
+
+    var $visualItem = $('.main-banner-img-inner');
+    var easingType = this.easingType;
+
+    var $pageItem;
+
+    var timeID, timeID2;
+    var imageMovingTime = 1000;
+    //var imageIntervalTime = 7000;
+    //var barStretchTime = 10;
+
+    // private
+    var _initPosition = function(){
+
+      $visualItem.hide().eq(0).show();
+
+    };
+
+    var _init = function(){
+
+      _initPosition();
+
+    };
+
+    var _setPlayButtonClass = function(status){
+      $('.main-visual-control-paging .play-button').attr('class', 'play-button').addClass(status);
+    };
+
+    // public
+    this.fade = function(){
+
+      if( nextVisualIndex >= $visualItem.length ){
+
+        nextVisualIndex = 0;
+
+      } else if( nextVisualIndex <= -1 ){
+
+        nextVisualIndex = $visualItem.length-1;
+
+      }
+
+      $visualItem.eq(currentVisualIndex).stop().fadeOut(imageMovingTime, easingType);
+      $visualItem.eq(nextVisualIndex).stop().fadeIn(imageMovingTime, easingType);
+
+      //$pageItem.find('.paging-link').removeClass('on');
+      //$pageItem.eq(nextVisualIndex).find('.paging-link').addClass('on');
+
+      currentVisualIndex = nextVisualIndex;
+
+    };
+
+    //this.rollAuto = function(){
+    //
+    //  var _fade = this.fade;
+    //
+    //  timeID = setInterval(function(){
+    //
+    //    nextVisualIndex = currentVisualIndex + 1;
+    //    _fade();
+    //
+    //    _timeBar(true);
+    //
+    //    //HeaderGnb.setClass(HeaderGnb.getCurrentMainSectionIndex(), nextVisualIndex);
+    //
+    //  }, imageIntervalTime);
+    //
+    //  _setPlayButtonClass('pause');
+    //
+    //};
+
+    this.rollLeft = function(){
+
+      this.rollStop();
+
+      nextVisualIndex = currentVisualIndex + 1;
+      this.fade();
+
+      //HeaderGnb.setClass(HeaderGnb.getCurrentMainSectionIndex(), nextVisualIndex);
+
+    };
+
+    this.rollRight = function(){
+
+      this.rollStop();
+
+      nextVisualIndex = currentVisualIndex - 1;
+      this.fade();
+
+      //HeaderGnb.setClass(HeaderGnb.getCurrentMainSectionIndex(), nextVisualIndex);
+
+    };
+
+    this.rollStop = function(){
+
+      // stop rolling
+      clearInterval(timeID);
+
+      // stop time bar
+      clearInterval(timeID2);
+
+      _setPlayButtonClass('play');
+
+    };
+
+    this.checkAnimate = function(){
+
+      return this.$mainVisualItem.is(':animated');
+
+    };
+
+    this.getNextVisualIndex = function(){
+
+      return nextVisualIndex;
+
+    };
+
+    // running in constructor when loading
+    _init();
+    //this.rollAuto();
 
   };
 
