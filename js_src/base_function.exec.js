0fad26ef53f3844cb7200da1e3f21f12ee154838
younsang <younsang.jo@gmail.com>
2018-02-10 16:33:52 +0900

    v3.0.2 2018/02/10 공사 메뉴/탭 반응형

--- js_src/base_function.exec.js
+++ js_src/base_function.exec.js
@@ -38,19 +38,7 @@ $(function(){
 
 
 
-    //$(window).on('resize', function(){
-    //
-    //  if( $(window).outerWidth() <= 1024 ){
-    //
-    //    if( $('.gnb-mobile-btn').length <= 0 ){
-    //      $('.header').prepend('<button type="button" class="gnb-mobile-btn btn-gnb"><span>주메뉴 열기</span></button>');
-    //    }
-    //
-    //  } else {
-    //    $('.gnb-mobile-btn.btn-gnb').remove();
-    //  }
-    //
-    //});
+
 
     var windowSize = $(window).outerWidth();
 
@@ -58,36 +46,58 @@ $(function(){
 
     if( windowSize <= 1080){
 
+      var depthOne = $('.left-menu-depth2'); //lnb
+      var depthTwo = $('.tab-nav-list'); //tab
+
+      var oneOnText = $('.left-menu-depth2-item-link.on').text();
+      var twoOnText = $('.tab-nav-list-link.on').text();
+      var threeOnText = $('.tab-nav.sub .tab-nav-list-link.on').text();
+
+      depthOne.prepend('<li class="menu-mobile-item"><button type="button" class="menu-mobile-ui"><span>메뉴선택</span></button></li>');
+      depthTwo.prepend('<li class="menu-mobile-item"><button type="button" class="menu-mobile-ui"><span>메뉴선택</span></button></li>');
+
+      $('.left-menu-depth2 .menu-mobile-ui span').text(oneOnText);
+      $('.tab-nav-list .menu-mobile-ui span').text(twoOnText);
+      $('.tab-nav.sub .tab-nav-list .menu-mobile-ui span').text(threeOnText);
+
+      $('.left-menu-depth2 .menu-mobile-ui').on('click', function(){
+          $(this).parent().siblings().toggleClass('open-ui');
+      });
+
+      $('.tab-nav-list .menu-mobile-ui').on('click', function(){
+        $(this).parent().siblings().toggleClass('open-ui');
+      });
+
       //$('.tab-nav-list-link.on').attr("href", "#");
       //$('.left-menu-depth2-item-link.on').attr("href", "#");
       //$('.tab-nav-list-link.on').attr("href", "#");
 
-      $('.tab-nav-list-link.on').on('click', function(e){
-        e.preventDefault();
-        $(this).parent().siblings().children().toggleClass('m-on');
+      //$('.tab-nav-list-link.on').on('click', function(e){
+      //  e.preventDefault();
+      //  $(this).parent().siblings().children().toggleClass('m-on');
+      //
+      //});
 
-      });
+      //$('.left-menu+.sub-contents .tab-nav:first').each(function() {
+      //  $('.left-menu+.sub-contents .tab-nav:first .tab-nav-list-item > .tab-nav-list-link.on').each(function(){
+      //    $(this).parent().detach().prependTo('.left-menu+.sub-contents .tab-nav:first .tab-nav-list'); // 해당 li를 떼어내서 #example의 처음에 끼운다. });
+      //  });
+      //});
 
-      $('.left-menu+.sub-contents .tab-nav:first').each(function() {
-        $('.left-menu+.sub-contents .tab-nav:first .tab-nav-list-item > .tab-nav-list-link.on').each(function(){
-          $(this).parent().detach().prependTo('.left-menu+.sub-contents .tab-nav:first .tab-nav-list'); // 해당 li를 떼어내서 #example의 처음에 끼운다. });
-        });
-      });
+      //$('.tab-nav.sub').each(function() {
+      //  $('.tab-nav.sub .tab-nav-list-item > .tab-nav-list-link.on').each(function(){
+      //    $(this).parent().detach().prependTo('.tab-nav.sub .tab-nav-list'); // 해당 li를 떼어내서 #example의 처음에 끼운다. });
+      //  });
+      //});
 
-      $('.tab-nav.sub').each(function() {
-        $('.tab-nav.sub .tab-nav-list-item > .tab-nav-list-link.on').each(function(){
-          $(this).parent().detach().prependTo('.tab-nav.sub .tab-nav-list'); // 해당 li를 떼어내서 #example의 처음에 끼운다. });
-        });
-      });
-
-      $('.left-menu-depth1-item-link.on').addClass('on');
-      $('.left-menu-depth2-item-link.on').on('click', function(e){
-        e.preventDefault();
-        $(this).parent().siblings().children().toggleClass('m-on');
-
-      });
+      //$('.left-menu-depth1-item-link.on').addClass('on');
+      //$('.left-menu-depth2-item-link.on').on('click', function(e){
+      //  e.preventDefault();
+      //  $(this).parent().siblings().children().toggleClass('m-on');
+      //
+      //});
     } else {
-
+      openUi.remove();
     }
 
 
